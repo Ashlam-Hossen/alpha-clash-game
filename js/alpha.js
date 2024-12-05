a7f1dd964fc73aced79c7a19f2e678aaaa6bb9e4
joy <joyhasan44@gmail.com>
2024-12-05 20:17:54 +0600

    all section compelete playground and development

--- js/alpha.js
+++ js/alpha.js
@@ -12,6 +12,84 @@
 //  playgroundSection.classList.remove('hidden');
 // }
 
+
+
+//-----------call back function----------
+function handleKeyButtonPress(event){
+   const playerPressed = event.key;
+    console.log('you pressed',playerPressed);
+
+ // sto the game if pressed 'ESC'  
+ if(playerPressed === 'Escape'){
+    gameOver();
+ } 
+
+// get the expectecd to press
+
+   const currentAlphabetElement= document.getElementById('current-alphabet');
+   const currentAlphabet = currentAlphabetElement.innerText;
+   const expectecdAlphabet = currentAlphabet.toLowerCase();
+   console.log(expectecdAlphabet,playerPressed);
+
+//    check matched or not 
+
+   if(playerPressed === expectecdAlphabet){
+    // console.log('you get the point');
+
+    const currentScore = getTextElementValueById('current-score');
+    console.log(currentScore);
+    const updateScore = currentScore + 1;
+    setTextElementValueById('current-score',updateScore);
+
+//-----------------------------------------------------------------------------------------------
+
+    //update score:
+    //1.get the current score
+    // const currentScoreElement = document.getElementById('current-score') ;
+    // const currentScoreText = currentScoreElement.innerText;
+    // const currentScore = parseFloat(currentScoreText);
+    // console.log(currentScore);
+
+    // //2.increase the score by 1 
+    // const newScore = currentScore + 1;
+    // //3.show the updated score
+    // currentScoreElement.innerText = newScore;
+
+    removeBackgroundColorById(expectecdAlphabet)
+    continueGame()
+   }
+   else{
+    console.log('you missed point');
+
+    const currentLife = getTextElementValueById('current-life');
+    const updateLife = currentLife - 1 ;
+
+    setTextElementValueById('current-life',updateLife);
+
+    if(updateLife ===0 ){
+        console.log('game is over');
+        gameOver();
+    }
+
+
+
+//------------------------------------------------------------------------------------------------
+    // // setp-1 get the current life number
+    // const currentLifeElement = document.getElementById('current-life');
+    // const currentLifeText = currentLifeElement.innerText;
+    // const currentLife = parseInt(currentLifeText);
+    // //setp - 2 reduce the life count
+    // const newLife = currentLife - 1;
+    // //display the  upaate life cound
+    // currentLifeElement.innerText = newLife;
+   }
+
+}
+//capture keyboard key press
+document.addEventListener('keyup',handleKeyButtonPress)
+
+
+
 function continueGame(){
 // step-1 genrate a random alphabet
  const alphabet = getRandomAlphabet();
@@ -30,7 +108,32 @@ setBackgroundColorById(alphabet);
 
 
 function playOnclik(){
+    // hide everthing show only the playground
+
     hideElementById('play-home');
+    hideElementById('final-score');
     showElementById('play-ground');
+
+    //rest  score and life
+    setTextElementValueById('current-life',5);
+    setTextElementValueById('current-score',0);
+
     continueGame();
+}
+
+function gameOver(){
+    hideElementById('play-ground');
+    showElementById('final-score');
+
+    //update final score
+    // 1.get the final score
+    const lastScore = getTextElementValueById('current-score');
+    console.log(lastScore);
+    setTextElementValueById('last-score',lastScore);
+
+    // clear the last selected alphabet heighlight
+
+    const currentAlphabet = getElementTextById('current-alphabet');
+    // console.log(currentAlphabet);
+    removeBackgroundColorById(currentAlphabet);
 }
--- js/utility.js
+++ js/utility.js
@@ -1,4 +1,5 @@
 
+// common function use for hidden elements
 // hide function
 function hideElementById(elementId){
     const element = document.getElementById(elementId);
@@ -10,12 +11,45 @@ function showElementById(elementId){
     const element = document.getElementById(elementId);
     element.classList.remove('hidden');
 }
-
+// common function use  for background
+//---------------------------------------------------------------------
 // set color function
 function setBackgroundColorById(elementId){
     const element = document.getElementById(elementId);
     element.classList.add('bg-orange-400');
 }
+
+
+function removeBackgroundColorById(elementId){
+    const element = document.getElementById(elementId);
+    element.classList.remove('bg-orange-400');
+}
+
+// common function use for score board
+
+function getTextElementValueById(elementId){
+    const element = document.getElementById(elementId);
+    const elementTextValue = element.innerText;
+    const elementValue = parseInt (elementTextValue);
+    return elementValue;
+}
+
+function getElementTextById(elementId){
+    const element = document.getElementById(elementId);
+    const text = element.innerText;
+    return text;
+}
+
+
+
+function setTextElementValueById(elementId,value){
+   const element = document.getElementById(elementId);
+   element.innerText = value;
+}
+
+
+
+
 
 
 
